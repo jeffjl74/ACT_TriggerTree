1f37292de034246bbbb55abb5c52c847e91e1964
jeffjl74 <jeffjl.on@outlook.com>
2021-07-30 12:01:17 -0500

    remove unused usings

--- Macros.cs
+++ Macros.cs
@@ -1,11 +1,7 @@
 ﻿using Advanced_Combat_Tracker;
-using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ACT_TriggerTree
 {
